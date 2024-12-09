803a402861854c18c81b6c31bac3f9ae11a29903
Yiming0576 <g1097420948@gmail.com>
2024-12-09 11:40:26 -0500

    remove the warnings

--- NorthWindTraders/src/main/java/com/pluralsight/northwindtraders/NorthWindTradersApplication.java
+++ NorthWindTraders/src/main/java/com/pluralsight/northwindtraders/NorthWindTradersApplication.java
@@ -1,16 +1,20 @@
 package com.pluralsight.northwindtraders;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class NorthWindTradersApplication {
 
+    private static final Logger log = LoggerFactory.getLogger(NorthWindTradersApplication.class);
+
     public static void main(String[] args) {
 
         ApplicationContext ac = SpringApplication.run(NorthWindTradersApplication.class, args);
+        log.info(String.valueOf(ac));
     }
 
 }
